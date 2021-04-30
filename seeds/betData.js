9a6a45072626711ef0936e7671a02de271b3080b
Maxson Green <maxson.green@gmail.com>
2021-04-29 20:56:46 -0400

    Created index.js for seed data and betData.js for seed data

--- seeds/betData.js
+++ seeds/betData.js
@@ -1,5 +1,23 @@
 const { Bet } = require('../models');
 
 const betData = [
-    
+    {
+        terms: 'Win two games of pool in a row',
+        prize: 'Buy dinner',
+        user_id: 1,
+        challenger_id: 2,
+    },
+    {
+        terms: 'Do not shave beard for 2 months',
+        prize: 'Wear opposing team jersey',
+        user_id: 3,
+        challenger_id: 4,
+    },
+    {
+        terms: 'Get a better grade on the next homework assignment',
+        prize: 'Buy a beer',
+        user_id: 3,
+        challenger_id: 1,
+    }
+
 ]
