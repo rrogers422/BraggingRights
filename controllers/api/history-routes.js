6f0e7d688c3bbcb65b58f30743363099ed0bf8f6
Somi Gupta <somigupta@sudharshans-MacBook-Pro.local>
2021-05-01 13:27:30 -0400

    Refactored history routes

--- controllers/api/history-routes.js
+++ controllers/api/history-routes.js
@@ -1,16 +1,19 @@
 const router = require('express').Router();
-// const path = require('path');
 const { User, History } = require('../../models');
 
+//route to get the wins/losses of a user
 router.get('/', async (req, res) => {
   try{
     const historyData = await History.findAll({
-    include: User 
-    })
+      attributes: ['wins', 'losses'],  
+    include: [ { 
+      model: User,
+      attributes: ['first_name', 'last_name']
+  }]
+  })
     res.status(200).json(historyData);
   }catch (err) {
-    console.log(err.message);
-    res.status(500).json(err);
+    res.status(500).json(err.message);
   }
 });
 
