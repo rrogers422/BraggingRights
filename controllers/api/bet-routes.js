fa40dc7fdff633967efaef52c86cd75498420b5f
Reagan Rogers <reagan422440@gmail.com>
2021-05-01 10:24:57 -0400

    updated bet/history routes

--- controllers/api/bet-routes.js
+++ controllers/api/bet-routes.js
@@ -1,8 +1,21 @@
 const router = require('express').Router();
+const Bet = require('../../models/Bet');
 const { bet } = require('../../models/Bet');
 
 router.get('/', async (req, res) => {
     try {
-        const betInfo 
+        const betData = await Bet.create(req.body);
+
+        req.session.save(() => {
+        res.status(200).json(betData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+);
+
+router.post('/', async (req, res) => {
+    try {
+        const betData = await Bet.findOne({ where: {}})
     }
 })
