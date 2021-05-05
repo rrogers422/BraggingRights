cba4b9c8370064907a534e0b73bcd24a0120f9bd
Somi Gupta <somigupta@sudharshans-MacBook-Pro.local>
2021-05-04 20:41:01 -0400

    Updated password seed data

--- seeds/userData.js
+++ seeds/userData.js
@@ -3,19 +3,23 @@ const User = require('../models/User');
 const userdata = [
     {
         username: 'Michael_Scott',
-        email: 'no1Boss@DunderMifflin.com'
+        email: 'no1Boss@DunderMifflin.com',
+        password: "password12345"
     },
     {
         username: 'Dwight_Schrute',
-        email: 'asstRegionalManager@DunderMifflin.com'
+        email: 'asstRegionalManager@DunderMifflin.com',
+        password: "password12345"
     },
     {
         username: 'Toby_Flenderson',
-        email: 'HRRep@DunderMifflin.com'
+        email: 'HRRep@DunderMifflin.com',
+        password: "password12345"
     },
     {
         username: 'Kelly_Kapoor',
-        email: 'marryMeRyan@DunnerMifflin.com'
+        email: 'marryMeRyan@DunnerMifflin.com',
+        password: "password12345"
     }
 ];
     
