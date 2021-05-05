f0cc0f0e966c82d3e634a206de226ccfc68237c1
Maxson Green <maxson.green@gmail.com>
2021-05-05 19:05:15 -0400

    Added /logout route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -58,6 +58,16 @@ router.post('/login', async (req, res) => {
         res.status(400).json(err);
     }
 
+    router.post('/logout', (req, res) => {
+        if (req.session.logged_in) {
+          req.session.destroy(() => {
+            res.status(204).end();
+          });
+        } else {
+          res.status(404).end();
+        }
+      });
+
 });
 
 // get route for create bet page
