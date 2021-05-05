f0d2b334617f71abc3ce383781e371cb9439f324
Reagan Rogers <reagan422440@gmail.com>
2021-05-05 17:46:19 -0400

    Debugging/Updated Routes/Added Homeroutes/fixed some handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,8 +4,15 @@ const historyRoutes = require('./history-routes');
 
 const betRoutes = require('./bet-routes');
 
+const userRoutes = require('./user-routes');
+
+
+
 router.use('/history', historyRoutes);
 
+
 router.use('/bets', betRoutes);
+
+router.use('/users', userRoutes)
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -9,6 +9,26 @@ router.get('/', (req, res) => {
     logged_in: req.session.logged_in
   });
 })
+
+
+router.get('/login', (req, res) => {
+  if(req.session.logged_in){
+    res.redirect("/");
+    return
+  };
+
+  res.render("login")
+});
+
+
+router.get('/signup', (req, res) => {
+  if(req.session.logged_in){
+    res.redirect("/");
+    return
+  };
+
+  res.render("signup")
+})
   
   
 
