fa1d9e069f3f905d0d80aa92370046da9153bfe6
Maxson Green <maxson.green@gmail.com>
2021-05-05 18:03:34 -0400

    Fixed userData.js and began post route for Bet

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,6 @@
 module.exports = {
     destruct_users: (users) => {
-        const  user1 = users[0].username;
+        const user1 = users[0].username;
         const user2 = users[1].username;
         return (user1 + " & " + user2);
     }  
