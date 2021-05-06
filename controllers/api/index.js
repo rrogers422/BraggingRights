2f170c6b4fdc2b2901bd0df6290462f8ce405368
Somi Gupta <somigupta@sudharshans-MacBook-Pro.local>
2021-05-06 11:11:05 -0400

    Updated home-routes and stats routes to get bets statistics

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 const historyRoutes = require('./history-routes');
-
 const betRoutes = require('./bet-routes');
+const statsRoutes = require('./stats-routes');
 
 router.use('/history', historyRoutes);
-
 router.use('/bets', betRoutes);
+router.use('/stats', statsRoutes);
 
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,15 +1,41 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Bet } = require('../models');
+const { Op } = require("sequelize");
+const axios = require('axios');
+
 // Middleware
 const withAuth = require('../utils/auth');
 
-//route for home
 router.get('/', (req, res) => {
-  res.render('home', {
-    logged_in: req.session.logged_in
-  });
+  res.render('home');
 })
   
-  
+// router.get('/', (req, res) =>{
+//   let userProfile = {};
+//     axios.get('/api/stats/pending/bets',
+//     {
+//       baseURL: `http://${req.get('host')}`
+//     }).then((res) => {
+//       console.log(res);
+//       userProfile.pending = res;
+//     }).catch(function (error) {
+//       console.log(error.message);
+//     })
+    
+//   // userProfile.active = await axios.get("/active/bets");
+//   // userProfile.wins = await axios.get("/win/bets");
+//   // userProfile.loss = await axios.get("/loss/bets");
+//   // res.render("home", { userProfile })
+// });
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
 
 module.exports = router;
