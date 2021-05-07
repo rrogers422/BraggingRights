60823d259a0404caec4a19b6619dd74581070585
Somi Gupta <somigupta@sudharshans-MacBook-Pro.local>
2021-05-06 21:44:13 -0400

    Fixed the session propagation

--- controllers/betStats-routes.js
+++ controllers/betStats-routes.js
@@ -9,15 +9,18 @@ router.get('/',withAuth, async(req, res) => {
           where: {
             status: {
               [Op.like]: 'accepted'
-            }
+            },
+            user_id: req.session.user_id
          },
+
     })
   
     let pendingBets = await Bet.findAndCountAll({
           where: {
             status: {
               [Op.like]: 'Not accepted'
-            }
+            },
+            user_id: req.session.user_id
          },
     })
     res.render('bet-stats', {
