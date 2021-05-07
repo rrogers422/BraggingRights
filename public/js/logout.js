3a9b60abe82349795ca073ad81f778a0670ec429
Reagan Rogers <reagan422440@gmail.com>
2021-05-07 19:19:45 -0400

    styling

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,13 +1,13 @@
-// const logout = async () => {
-//     const response = await fetch('/api/users/logout', {
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json' },
-//     });
-//     if (response.ok) {
-//       document.location.replace('/');
-//     } else {
-//       console.log("Error!");
-//     }
-//   };
+const logout = async () => {
+    const response = await fetch('/api/users/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      console.log("Error!");
+    }
+  };
   
-//   document.querySelector('#logout').addEventListener('click', logout);
+  document.querySelector('#logout').addEventListener('click', logout);
