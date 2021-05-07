b39e0c3416e1b683dca3ca95b8b944fa1e976b2c
Somi Gupta <somigupta@sudharshans-MacBook-Pro.local>
2021-05-07 19:34:14 -0400

    Fixed history routes

--- seeds/index.js
+++ seeds/index.js
@@ -1,19 +1,16 @@
 const sequelize = require('../config/connection');
-// const seedUsers = require('./userData');
+const seedUsers = require('./userData');
 const seedBets = require('./betData');
 const seedHistory = require('./historyData');
-const seedUserBet = require('./UserBetData');
 
 const seedAll = async () => {
     await sequelize.sync({force: true});
 
-    // await seedUsers();
+    await seedUsers();
 
     await seedBets();
 
     await seedHistory();
-
-    // await seedUserBet();
 
     process.exit(0);
 };
